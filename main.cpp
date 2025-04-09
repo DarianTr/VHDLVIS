51dc25b6e07c1d9d7f2069520e896d95e50927df
DarianTr <darian@tratar.de>
2025-04-09 15:48:24 +0200

    first semantic checks are working

--- main.cpp
+++ main.cpp
@@ -6,9 +6,43 @@
 #include "antlr4-runtime.h"
 #include "vhdlLexer.h"
 #include "vhdlParser.h"
+#include "vhdlBaseListener.h"
+
 #include <iostream>
+#include <map>
 
 using namespace antlr4;
+
+enum DeclarationTypes {
+    Entity,
+};
+
+std::map<std::string, std::set<DeclarationTypes>> global_declaration_table;
+
+class GlobalDeclarationListener : public vhdlBaseListener {
+
+    void exitEntity_declaration(vhdlParser::Entity_declarationContext * ctx) override {
+        global_declaration_table[ctx->identifier().back()->getText()].insert(Entity);
+        if (ctx->identifier()[0]->getText() != ctx->identifier().back()->getText()) {
+            std::cerr << "Start and End Identifier aren't matching. Start is " << ctx->identifier()[0]->getText() << " and End ist " << ctx->identifier().back()->getText() << std::endl;
+        }
+    }
+
+    void exitArchitecture_body(vhdlParser::Architecture_bodyContext * ctx) override {
+        if (ctx->identifier()[0]->getText() != ctx->identifier().back()->getText()) {
+            std::cerr << "Start and End Identifier aren't matching. Start is " << ctx->identifier()[0]->getText() << " and End ist " << ctx->identifier().back()->getText() << std::endl;
+        }
+    }
+
+};
+
+class DeclaredBeforeUsedListener : public vhdlBaseListener {
+    void exitArchitecture_body(vhdlParser::Architecture_bodyContext * ctx) override {
+        if (!global_declaration_table[ctx->identifier()[1]->getText()].count(Entity)) {
+            std::cerr << "No Entity " << ctx->identifier()[1]->getText() << std::endl;
+        }
+    }
+};
 
 void printTree(antlr4::tree::ParseTree *tree, const std::string &indent = "", bool last = true) {
     std::string marker = last ? "└── " : "├── ";
@@ -29,14 +63,30 @@ int main() {
         std::cout << "opened" << std::endl;
     }
 
-    ANTLRInputStream input(stream);
-    vhdlLexer lexer(&input);
-    CommonTokenStream tokens(&lexer);
-    vhdlParser parser(&tokens);
+    // First pass - fill declaration table
+    {
+        std::ifstream stream("../input.txt");
+        ANTLRInputStream input(stream);
+        vhdlLexer lexer(&input);
+        CommonTokenStream tokens(&lexer);
+        vhdlParser parser(&tokens);
+        auto L = new GlobalDeclarationListener();
+        parser.addParseListener(L);
+        tree::ParseTree *tree = parser.design_file();
+    }
 
-    tree::ParseTree *tree = parser.design_file();
-    // std::cout << tree->toStringTree(&parser) << std::endl;
-    printTree(tree);
+    // Second pass - check for declaration before use
+    {
+        std::ifstream stream("../input.txt");
+        ANTLRInputStream input(stream);
+        vhdlLexer lexer(&input);
+        CommonTokenStream tokens(&lexer);
+        vhdlParser parser(&tokens);
+        auto L = new DeclaredBeforeUsedListener();
+        parser.addParseListener(L);
+        tree::ParseTree *tree = parser.design_file();
+    }
+
 
     return 0;
 }
